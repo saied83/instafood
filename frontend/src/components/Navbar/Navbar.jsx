e148e21afd23e3fcbf16eb0831b8b39820525908
saied83 <saiedtechit@gmail.com>
2024-09-02 01:39:46 +0600

    starting cart page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -65,7 +65,9 @@ const Navbar = ({ setShowLogin }) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
+          <Link to={"/cart"}>
+            <img src={assets.basket_icon} alt="" />
+          </Link>
           <div className="dot"></div>
         </div>
         <button
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Cart.css";
+import { useStoreContext } from "../../context/StoreContext";
+import { food_list } from "../../assets";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const { cartItems, foot_list, addToCart, removeFromCart } = useStoreContext();
+  return (
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {})}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
