fc1ebf0988a685ac491996ea66235012538e0d93
saied83 <saiedtechit@gmail.com>
2024-09-02 13:21:41 +0600

    create place-order page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -69,7 +69,13 @@ const Cart = () => {
               <b>${getTotalCartAmount() + 2}</b>
             </div>
           </div>
-          <button>PROCEED TO CHECKOUT</button>
+          <button
+            onClick={() => {
+              navigate("/order");
+            }}
+          >
+            PROCEED TO CHECKOUT
+          </button>
         </div>
         <div className="cart-promocode">
           <div>
